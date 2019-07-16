4731452cd6c115686b65e3d2f175e3c9496db1f3
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2019-07-16 14:33:39 +0900

    spark_Test

--- twitterStreaming/src/main/scala/kr/kookmin/hjkimSpkStreaming/SparkInst.scala
+++ twitterStreaming/src/main/scala/kr/kookmin/hjkimSpkStreaming/SparkInst.scala
@@ -39,7 +39,7 @@ object SparkInst{
 
   }
 
-  private def wordCount_Example(ssc): Unit = {
+  private def wordCount_Example(ssc: Unit): Unit = {
     var lines = ssc.socketTextStream("10.160.0.4", 9999);
     var words = lines.flatMap(_.split(" "));
 
