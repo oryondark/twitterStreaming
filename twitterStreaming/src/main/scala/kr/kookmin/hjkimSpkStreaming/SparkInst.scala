cd12c7b276a959d2770bd6783eea2886437f4099
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2019-07-16 14:24:22 +0900

    spark_Test

--- twitterStreaming/src/main/scala/kr/kookmin/hjkimSpkStreaming/SparkInst.scala
+++ twitterStreaming/src/main/scala/kr/kookmin/hjkimSpkStreaming/SparkInst.scala
@@ -15,7 +15,14 @@ object SparkInst{
   def main(args: Array[String]): Unit = {
 
     var keys = parseKey(args);
-    println(keys)
+    println(keys.mkString(","));
+
+    //Spark Streaming Context
+    var ssc = new StreamingContext("10.160.0.4", "NetworkWordCount", Seconds(1));
+    wordCount_Example(ssc);
+    ssc.start();
+    ssc.awaitTermination();
+
     /*
     var twitter_instance = new TwitterFactory().getInstance()
     //Consumer auth assigning for accessing twitter
@@ -32,11 +39,21 @@ object SparkInst{
 
   }
 
+  private def wordCount_Example(ssc) {
+    var lines = ssc.socketTextStream("10.160.0.4", 9999);
+    var words = lines.flatMap(_.split(" "));
+
+    var pairs = words.map(word => (word, 1));
+    var wordCounts = pairs.reduceByKey(_ + _);
+
+    wordCounts.print()
+  }
+
   private def parseKey(args: Array[String]): Array[String] = {
-    var consumer_pubKey: String = ""
-    var consumer_secKey: String = ""
-    var accessToken_pub: String = ""
-    var accessToken_sec: String = ""
+    var consumer_pubKey: String = args(0);
+    var consumer_secKey: String = args(1);
+    var accessToken_pub: String = args(2);
+    var accessToken_sec: String = args(3);
 
     return Array(consumer_pubKey, consumer_secKey, accessToken_pub, accessToken_sec)
   }
