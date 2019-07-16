33c162ed9a135ab932a9f202c29d609abcc9ecaa
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2019-07-16 13:44:35 +0900

    spark_Test

--- twitterStreaming/src/main/scala/kr/kookmin/hjkimSpkStreaming/scalaTest.scala
+++ twitterStreaming/src/main/scala/kr/kookmin/hjkimSpkStreaming/scalaTest.scala
@@ -2,19 +2,21 @@ package kr.kookmin.hjkimSpkStreaming
 import twitter4j.TwitterFactory
 import twitter4j.auth.AccessToken
 
-/**
- * @author ${user.name}
- */
-object scalaTest{
-  
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.api.java.function._
 
+
+object SparkInst{
+  /*
+  Spark Streaming Usage:
+  Please, inputs the arguments keys to access your twitter.
+  */
   def main(args: Array[String]): Unit = {
 
-    var consumer_pubKey: String = ""
-    var consumer_secKey: String = ""
-    var AccessToken_pub: String = ""
-    var AccessToken_sec: String = ""
-
+    var keys = parseKey(args);
+    println(keys)
+    /*
     var twitter_instance = new TwitterFactory().getInstance()
     //Consumer auth assigning for accessing twitter
     twitter_instance.setOAuthConsumer(consumer_pubKey, // Consummer key
@@ -26,6 +28,16 @@ object scalaTest{
 
     var listen_homeTimeLine = twitter_instance.getHomeTimeline
     println(listen_homeTimeLine)
+    */
 
+  }
+
+  private def parseKey(args: Array[String]): Array[String] = {
+    var consumer_pubKey: String = ""
+    var consumer_secKey: String = ""
+    var AccessToken_pub: String = ""
+    var AccessToken_sec: String = ""
+
+    return (consumer_secKey, consumer_secKey, AccessToken_pub, AccessToken_sec)
   }
 }
